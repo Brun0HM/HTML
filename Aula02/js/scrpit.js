3961f9750dd3749664c1727cd3dc556ac013a9bf
Bruno <158314045+Brun0H28@users.noreply.github.com>
2024-03-26 10:17:11 -0300

    Aula 26/03 Antes intervalo

--- Aula02/js/scrpit.js
+++ Aula02/js/scrpit.js
@@ -1,3 +1,15 @@
 alerta = () => {
-    alert("Olá eu sou um alerta")
-}
+  alert("Olá eu sou um alerta");
+};
+
+const enviar = () => {
+  let nome = document.getElementById("nome").value;
+  document.getElementById(
+    "resposta"
+  ).innerHTML = `<h2>Olá ${nome}, formulário enviado com sucesso!</h2>`;
+  if (nome == "") {
+    alert("Bem vindo!");
+  } else {
+    alert(`Olá ${nome}, bem vindo!`);
+  }
+};
